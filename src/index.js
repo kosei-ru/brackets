8286a112d7213c137070758bc6abd5a880d44a3f
kosei-ru <kosei.ru@gmail.com>
2023-02-12 22:50:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let length = str.length;
+
+  function recursion (pos) {
+    if (pos < 0) return pos;
+
+    let elem1 = str[pos];
+    let elem2 = '';
+
+    for (i = 0; i < bracketsConfig.length; i++)
+      if (bracketsConfig[i][0] == elem1) elem2 = bracketsConfig[i][1];
+
+    if (elem2 == '') return -1;
+
+    pos++;
+    while (true) {
+      if (pos == length) return -1;
+      if (pos == -1) return -1;     
+      if (str[pos] == elem2) return pos + 1
+      else pos = recursion (pos);
+    }
+  }
+
+  let i = 0;
+  while (i < length) {
+    i = recursion(i);
+    if (i < 0) return false;
+  }
+
+  return true;
 }
